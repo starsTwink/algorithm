cee2db0e4e4132a82e852184e271b0967f7c7d8a
xingxiaochi <2357299623@qq.com>
2020-05-05 23:15:52 +0800

    算法学习

---
+++ src/main/java/com/ghx/math/study/ArrayContain.java
@@ -0,0 +1,67 @@
+package com.ghx.math.study;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author：ghx
+ * @date：2020/5/2 0:51
+ * @describe：判断数组中是否含有该整数
+ */
+public class ArrayContain {
+
+    public static Logger logger = LoggerFactory.getLogger(ArrayContain.class);
+//    public static boolean Find(int target, int [][] array) {
+//        for (int[]m:array){
+//            for(int n:m){
+//                if(target==n){
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
+//    }
+
+    public static boolean Find(int target, int [][] array) {
+
+        /*
+        思路：
+        根据数组的特点可以发现,如果从左下角开始查找更为方便
+        左下角的数比同一列的数大，同时比同一行的数小
+        也就是从左下角开始比，如果target传入的数大于它则列数加一继续比较
+        如果小于它则行数减一继续比较
+        */
+        //定义行数
+        int rows=array.length;
+        //定义列数
+        int lies=array[0].length;
+        //定义i用来技计数
+        int i=0;
+        //while里的条件是极限情况，不满足条件的时候则停止
+        while((rows>0)&&(i<lies))
+        {
+            //目标大于左下角，让列数自增
+            if(target>array[rows-1][i])
+            {
+                logger.info(""+array[rows-1][i]);
+                i++;
+                //目标小于左下角，让行数自减
+            }else if(target<array[rows-1][i])
+            {
+                rows--;
+            }else
+            {
+                //除了上述两种情况就是相等了，说明已经找到了
+                return true;
+            }
+        }
+        //遍历完还没有找到，说明不存在
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr={{12,58},{56,78,},{0,41}};
+        boolean find = Find(40, arr);
+        logger.info("Find:"+find);
+       }
+}
---
+++ src/main/java/com/ghx/math/study/ReplaceString.java
@@ -0,0 +1,49 @@
+package com.ghx.math.study;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author：ghx
+ * @date：2020/5/2 23:14
+ * @describe：空格替换为%20
+ */
+public class ReplaceString {
+
+    public static Logger logger = LoggerFactory.getLogger(ReplaceString.class);
+
+//    public String replaceSpace(StringBuffer str) {
+//        String replace = str.toString().replace(" ", "%20");
+//           return replace;
+//    }
+
+    public String replaceSpace(StringBuffer str) {
+        int spacenum = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == ' '){
+                spacenum++;
+            }
+        }
+        int oldLength = str.length();
+        int oldIndex = oldLength - 1;
+        int newLength = oldLength + spacenum*2;
+        str.setLength(newLength);
+        int newIndex = newLength - 1;
+        for(; oldIndex >= 0 && oldLength < newLength; oldIndex--){
+            if(str.charAt(oldIndex) == ' '){
+                str.setCharAt(newIndex--, '0');
+                str.setCharAt(newIndex--, '2');
+                str.setCharAt(newIndex--, '%');
+            }else{
+                str.setCharAt(newIndex--, str.charAt(oldIndex));
+            }
+        }
+        return str.toString();
+    }
+
+    public static void main(String[] args) {
+        StringBuffer str = new StringBuffer("apple   d");
+        String replace = str.toString().replace(" ", "%20");
+        logger.info(""+replace);
+    }
+}
