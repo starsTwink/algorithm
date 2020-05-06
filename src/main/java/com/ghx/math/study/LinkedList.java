130ed5fcc465ded4f7a5d5e634132c2b0c49173d
xingxiaochi <2357299623@qq.com>
2020-05-06 23:35:43 +0800

    从尾到头打印链表

---
+++ src/main/java/com/ghx/math/study/LinkedList.java
@@ -0,0 +1,26 @@
+package com.ghx.math.study;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author：ghx
+ * @date：2020/5/5 22:46
+ * @describe：从尾到头打印链表
+ */
+public class LinkedList {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        while (listNode!=null){
+            list.add(listNode.val);
+            listNode=listNode.next;
+        }
+        for( int i = list.size()-1; i>=0; i--){
+             result.add(list.get(i));
+        }
+        return  result;
+    }
+}
---
+++ src/main/java/com/ghx/math/study/ListNode.java
@@ -0,0 +1,19 @@
+package com.ghx.math.study;
+
+/**
+ * @author：ghx
+ * @date：2020/5/5 22:50
+ * @describe：
+ */
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+}
