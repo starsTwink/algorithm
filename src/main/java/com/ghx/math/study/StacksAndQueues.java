81ce12c87b1a8e075706c2070a1ab5d58c43bb64
xingxiaochi <2357299623@qq.com>
2020-05-09 10:50:23 +0800

    用两个栈实现队列

---
+++ src/main/java/com/ghx/math/study/StacksAndQueues.java
@@ -0,0 +1,42 @@
+package com.ghx.math.study;
+
+import java.util.Stack;
+
+/**
+ * @author：ghx
+ * @date：2020/5/7 14:33
+ * @describe：用两个栈实现队列
+ * 栈是先进后出
+ * 队是先进先出
+ */
+public class StacksAndQueues {
+
+        Stack<Integer> stack1 = new Stack<Integer>();
+        Stack<Integer> stack2 = new Stack<Integer>();
+
+        public void push(int node) {
+            while(!stack2.isEmpty()){
+                stack1.push(stack2.pop());
+            }
+            stack1.push(node);
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+
+        public int pop() {
+            return stack2.pop();
+        }
+        public static void main(String[] args) {
+            // TODO Auto-generated method stub
+            StacksAndQueues s = new StacksAndQueues();
+            s.push(1);
+            s.push(2);
+            s.push(3);
+            s.push(4);
+            s.push(5);
+            int pop = s.pop();
+            System.out.println(pop);
+        }
+
+}
