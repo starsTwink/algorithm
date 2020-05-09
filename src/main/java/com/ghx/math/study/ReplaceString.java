4e0c5a443f1f4fef59390ec9cee8536074547102
xingxiaochi <2357299623@qq.com>
2020-05-09 11:28:20 +0800

    替换空格

--- src/main/java/com/ghx/math/study/ReplaceString.java
+++ src/main/java/com/ghx/math/study/ReplaceString.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
  * @author：ghx
  * @date：2020/5/2 23:14
  * @describe：替换空格
+ * 请实现一个函数，将一个字符串中的每个空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
  */
 public class ReplaceString {
 
